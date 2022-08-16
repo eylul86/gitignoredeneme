5690e72de0ee3a94571522d89fc434f5d6f49f50
Eylül Mavi <mavigokyuzumavi5@gmail.com>
2022-08-16 22:54:46 +0300

    ttt

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -2,6 +2,7 @@ public class GitIgnore {
     public static void main(String[] args) {
 
         System.out.println("git ıgnore deneme 1");
+        System.out.println("git ıgnore deneme abc");
 
 
     }
